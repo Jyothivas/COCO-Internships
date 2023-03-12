83bfb14f60000f847cb276af2398787faa32e8bd
amruta-07 <misalamruta08@gmail.com>
2023-03-12 13:18:31 +0530

    Done with all unit tests and carousel design on homeImages

--- ReactNative/JsonData/BuyData/App.tsx
+++ ReactNative/JsonData/BuyData/App.tsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, FC } from 'react';
 import { FlatList, Text, View, Image, ScrollView } from 'react-native';
 import Jsondata from './Assests/listings-buy-data.json';
 import { Dimensions } from 'react-native';
 
-const width = Dimensions.get("window").width
+interface movidataprops {
+  movidata: any
+}
+const width = Dimensions.get('window').width;
 
-const App = () => {
+const App: FC<movidataprops> = (props) => {
 
 
-  const [movidata] = useState(Jsondata.data.listings.data)
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [movidata] = useState(props.movidata || Jsondata.data.listings.data);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <View style={{ flex: 1 }}>
       <FlatList
         data={movidata.slice(0, 30)}
+        ListEmptyComponent={<Text>No data available</Text>}
 
-        renderItem={({ item }) => {
+        renderItem={({ item, index }) => {
 
           return (
-            <View style={{ width: '90%', alignSelf: 'center', }}>
-              <Text style={{ fontSize: 15 }}>heroImage</Text>
+            <View style={{ width: '90%', alignSelf: 'center' }}>
+              <Text style={{ fontSize: 15 }} testID="hero-image-text" >heroImage</Text>
               <Image
 
                 style={{ height: 120, width: '80%' }}
@@ -28,48 +32,53 @@ const App = () => {
                   uri: 'https://cdn.uatr.view.com.au/images/listing/slug/800-min/' + item.heroImageUrl.split("/")[2]
 
                 }}
+                testID={'heroImage' + index}
 
               />
-              <Text style={{ fontSize: 15 }}>agentPhotoImage</Text>
+              <Text style={{ fontSize: 15 }} testID="agent-image-text">agentPhotoImage</Text>
               {
-                item.agents.map((agentItem: any, index: number) => {
+                item.agents.map((agentItem: any, agentindex: number) => {
                   return (
 
                     <Image
-                      key={'agentPhotoImage' + index}
+
+                      key={'agentPhotoImage' + agentindex}
                       style={{ height: 120, width: '100%', marginTop: 5 }}
                       source={{
                         uri: 'https://cdn.uatr.view.com.au/images/listing/55-w/' + agentItem.agentPhotoFileName
 
                       }}
+                      testID={'agentPhotoImage' + agentindex + index}
                     />
 
-                  )
+                  );
                 })
               }
-              <Text> HouseImage</Text>
+              <Text testID="house-image-text"> HouseImage</Text>
               <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={true}
                 onScroll={e => {
                   const x = e.nativeEvent.contentOffset.x;
                   setActiveIndex(Number((x / width).toFixed(0)));
 
                 }}
+
+                testID={'houseImagesScrollView' + index}
               >
-                <View style={{ flexDirection: 'row', }}>
+                <View style={{ flexDirection: 'row' }}>
                   {
-                    item.images.map((houseimg: any, index: number) => {
+                    item.images.map((houseimg: any, houseindex: number) => {
 
                       return (
                         <View style={{ width: width * 0.9, height: 150, marginLeft: 5 }}
-                          key={'HouseImage' + index}
-                        >
+                          key={'HouseImage' + houseindex}>
                           <Image
-
                             style={{ height: 150, width: '100%', marginTop: 5 }}
                             source={{
                               uri: 'https://cdn.uatr.view.com.au/images/listing/slug/800-min/' + houseimg.url.split("/")[2]
 
                             }}
+                            testID={'houseImage' + houseindex + index}
+
                           />
                         </View>
                       )
@@ -78,14 +87,16 @@ const App = () => {
                 </View>
 
               </ScrollView>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center'
-                }}>
+              <View style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+
+              >
                 {
-                  item.images.map((houseimg: any, index: number) => {
+
+                  item.images.map(() => {
                     return (
                       <View style={{
                         width: activeIndex == index ? 15 : 8,
@@ -93,17 +104,16 @@ const App = () => {
                         borderRadius: activeIndex == index ? 5 : 4,
                         backgroundColor: activeIndex == index ? 'green' : 'gray',
                         marginLeft: 5,
-                        marginTop: 8
-                      }}>
-
-                      </View>
-                    )
+                        marginTop: 8,
+                      }}
+                      />
+                    );
                   })
                 }
 
               </View>
             </View>
-          )
+          );
         }}
         keyExtractor={(item, index) => index.toString()}
       />
@@ -111,7 +121,7 @@ const App = () => {
     </View>
 
 
-  )
-}
+  );
+};
 
 export default App
--- ReactNative/JsonData/BuyData/__tests__/App-test.tsx
+++ ReactNative/JsonData/BuyData/__tests__/App-test.tsx
@@ -1,13 +1,35 @@
-
-
-import 'react-native';
-import React from 'react';
+import { render } from '@testing-library/react-native';
+import Jsondata from '../Assests/listings-buy-data.json';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+let movidata = Jsondata.data.listings.data.slice(0, 10)
+describe('App component', () => {
+  it('renders the component without errors', () => {
+    render(<App movidata={movidata} />);
+  });
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('renders when the data is empty', () => {
+    const { queryByText } = render(<App movidata={[]} />);
+    const emptyDataText = queryByText('No data available');
+    expect(emptyDataText).not.toBeNull();
+  });
+
+  it('renders and scrolls through images', () => {
+    const { getByTestId } = render(<App movidata={movidata} />);
+
+    // Check that the hero image, agent photos, and house images are rendered
+    movidata.map((item: any, index: number) => {
+      const heroImage = getByTestId('heroImage' + index);
+      expect(heroImage).toBeTruthy();
+
+      item.agents.map((agentItem: any, agentindex: number) => {
+        const agentPhotoimage = getByTestId('agentPhotoImage' + agentindex + index);
+        expect(agentPhotoimage).toBeTruthy();
+      });
+      item.images.map((houseimg: any, houseindex: number) => {
+        const housePhotoimage = getByTestId('houseImage' + houseindex + index);
+        expect(housePhotoimage).toBeTruthy();
+      });
+    });
+  });
 });
-
